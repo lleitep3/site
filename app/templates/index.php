000fa4d42b0960d32d9d9cb84a252fca1ccf1c94
Leandro Leite <leandro@leandroleite.info>
2015-07-12 01:36:25 -0300

    adicionado analytics

--- app/templates/index.php
+++ app/templates/index.php
@@ -56,5 +56,14 @@
 
       </div>
     </div>
+     <script>
+       !function(A,n,g,u,l,a,r){A.GoogleAnalyticsObject=l,A[l]=A[l]||function(){
+       (A[l].q=A[l].q||[]).push(arguments)},A[l].l=+new Date,a=n.createElement(g),
+       r=n.getElementsByTagName(g)[0],a.src=u,r.parentNode.insertBefore(a,r)
+       }(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+       ga('create', 'UA-XXXXX-X');
+       ga('send', 'pageview');
+    </script>
   </body>
 </html>
